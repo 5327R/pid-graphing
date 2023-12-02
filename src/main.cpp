a84b0ffaf59bb2ba4da182b6c3dd20b69969d15b
nakulsoneji <nakulsoneji@gmail.com>
2023-12-01 22:03:14 -0800

    experimenting with matplot++ backends

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "matplot/backend/gnuplot.h"
+#include "matplot/backend/opengl.h"
+#include "matplot/util/handle_types.h"
 #include <matplot/matplot.h>
 #include <numeric>
 #include <vector>
@@ -12,7 +15,9 @@ double process_input(double error, std::vector<double> &prev, double Kp,
 }
 
 std::vector<double> test_vals(double Kp, double Ki, double Kd,
+                              matplot::figure_handle fig,
                               int max_points = 500) {
+
   double target = 1000;
   double start = 0.01;
   double incr = 0.01;
@@ -20,9 +25,14 @@ std::vector<double> test_vals(double Kp, double Ki, double Kd,
   double time = 0;
   std::vector<double> p;
   std::vector<double> y;
+  std::vector<double> x;
+
+  auto ax = fig->current_axes();
 
   for (int i = 0; i < max_points; i++) {
+
     y.push_back(start);
+    x.push_back(i);
     err = target - start;
     incr = process_input(err, p, Kp, Ki, Kd);
     start += incr;
@@ -33,23 +43,26 @@ std::vector<double> test_vals(double Kp, double Ki, double Kd,
 }
 
 int main() {
-  std::vector<double> test = test_vals(0.1, 0.0001, 0.005);
-  std::vector<double> test2 = test_vals(1.0, 0.01, 0.05);
-  std::vector<double> test3 = test_vals(0.01, 0.02, 0.5);
+  /**/
+  auto fig = matplot::figure<matplot::backend::gnuplot>(true);
 
-  matplot::figure_type f;
+  auto ax = fig->current_axes();
 
-  /* auto ax = f.add_axes(); */
-  auto ax = matplot::nexttile();
+  std::vector<double> test = test_vals(0.1, 0.0001, 0.005, fig);
+  std::vector<double> test2 = test_vals(1.0, 0.01, 0.05, fig);
+  std::vector<double> test3 = test_vals(0.01, 0.02, 0.5, fig);
+  /* ax->plot(matplot::iota(0, 100), test); */
+  /* ax->hold(matplot::on); */
+  /* ax->plot(matplot::iota(0, 100), test2); */
+  /* ax->plot(matplot::iota(0, 100), test3); */
+  ax->plot(test);
+  ax->hold(true);
+  ax->plot(test2);
+  ax->plot(test3);
 
-  matplot::xlabel(ax, "Time");
-  matplot::ylabel(ax, "Position");
+  fig->show();
 
-  matplot::plot(test);
-  matplot::hold(matplot::on);
-  matplot::plot(test2);
-  matplot::plot(test3);
-  matplot::show();
+  /**/
 
   return 0;
 }
